69667406e9db9eeb0a40864b5e0c28d4de32543a
Shakhnoza Normamatova <normamatovashakhnoza@gmail.com>
2025-09-28 13:01:08 +0500

    registration

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import home, logout_user, register_user
+
+urlpatterns = [
+    path("", home, name="home"),
+    # path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', register_user, name='register'),
+]
